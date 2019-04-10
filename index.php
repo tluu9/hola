88115474c5b9c8ef2a67d42ee0b0704567542b43
Trang Luu <tluu9@mail.greenriver.edu>
2019-04-10 16:18:50 -0400

    Added Fat-free error reporting

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+/** Another test project...*/
+/**Trang Luu
+ *10 April 2019
+ */
 
 //Turn on error reporting
 ini_set('display_errors', 1);
@@ -10,7 +14,10 @@ require_once('vendor/autoload.php');
 //Create an instance of the Base class/ instantiate Fat-Free
 $f3 = Base::instance();
 
-//Define a default route
+//Turn on Fat-free error reporting/Debugging
+$f3->set('DEBUG',3);
+
+//Define a default route (use backlash / )
 $f3->route('GET /', function()
 {
     echo'<h1>Hola!</h1>';
